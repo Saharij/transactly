fd241e3c19ec9d783a52170634bdf5ecb99aa690
Zakhar Mychka <zakharii.mychka@plus4u.net>
2025-03-18 22:43:36 +0200

    minor firebase changes

--- src/models/firebase/firebase-config.ts
+++ src/models/firebase/firebase-config.ts
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+// import { getAnalytics } from 'firebase/analytics'
 import { collection, getFirestore, doc, setDoc, getDoc, getDocs } from 'firebase/firestore'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAAa5M-hHrkaOWqR4Tl4_FHFssqew4qq80',
-  authDomain: 'transactly-6aa07.firebaseapp.com',
-  projectId: 'transactly-6aa07',
-  storageBucket: 'transactly-6aa07.firebasestorage.app',
-  messagingSenderId: '949200998533',
-  appId: '1:949200998533:web:793e12d6a11bfb893f19ec',
-  measurementId: 'G-HYZVTEPQ4K',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 // const analytics = getAnalytics(app);
 
